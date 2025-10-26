dc35b7392472ff1f6aea3c6a9a0238c6bc7eb40f
Mohammad <immohammad20000@protonmail.com>
2025-10-26 11:16:51 +0330

    fix

--- app/subscription/xray.py
+++ app/subscription/xray.py
@@ -400,7 +400,7 @@ class XrayConfiguration(BaseSubscription):
             user_settings=user_settings,
         )
 
-    def _build_shadowsocks(self, address: str, inbound: SubscriptionInboundData, settings: dict) -> dict:
+    def _build_shadowsocks(self, address: str, remark: str, inbound: SubscriptionInboundData, settings: dict) -> dict:
         """Build Shadowsocks outbound"""
         method, password = self.detect_shadowsocks_2022(
             inbound.is_2022,
