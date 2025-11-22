45633575ebff693b73fd08d10e5ec2fe979f78ef
Mohammad <immohammad20000@protonmail.com>
2025-11-22 13:50:02 +0330

    fix(validation): update status code for validation exception response to HTTP_422_UNPROCESSABLE_CONTENT

--- app/__init__.py
+++ app/__init__.py
@@ -115,6 +115,6 @@ def validation_exception_handler(request: Request, exc: RequestValidationError):
     for error in exc.errors():
         details[error["loc"][-1]] = error.get("msg")
     return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
         content=jsonable_encoder({"detail": details}),
     )
