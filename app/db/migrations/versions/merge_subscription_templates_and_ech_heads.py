84f7fbf337a0211601cd5633018eb5a35ab3297c
Oleg German <oleg.german@gmail.com>
2026-03-04 01:10:39 +0300

    fix: add merge migration for multiple Alembic heads

---
+++ app/db/migrations/versions/merge_subscription_templates_and_ech_heads.py
@@ -0,0 +1,24 @@
+"""merge subscription_templates and ech_query_strategy heads
+
+Revision ID: a1b2c3d4e5f6
+Revises: f1c2d3e4b5a6, 2f3179c6dc49
+Create Date: 2026-03-03 22:15:00.000000
+
+"""
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "a1b2c3d4e5f6"
+down_revision = ("f1c2d3e4b5a6", "2f3179c6dc49")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
