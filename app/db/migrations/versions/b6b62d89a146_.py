688fb61bb33ac6b48e85437b9399e2058c8bd13a
Mohammad <immohammad20000@protonmail.com>
2026-06-15 19:09:53 +0330

    fix migrations

---
+++ app/db/migrations/versions/b6b62d89a146_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b6b62d89a146
+Revises: a3b4c5d6e7f8, c9b48df42f10
+Create Date: 2026-06-15 19:09:11.654738
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b6b62d89a146'
+down_revision = ('a3b4c5d6e7f8', 'c9b48df42f10')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
